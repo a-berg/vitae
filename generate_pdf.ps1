b2e1a539dc077556912d7669e355925ad09f80b5
a-berg <aberges@outlook.com>
2019-08-11 18:21:04 +0200

    Finished CV template and published the updated version as of Aug 2019

--- generate_pdf.ps1
+++ generate_pdf.ps1
@@ -1,1 +1,4 @@
-pdflatex src/cv.tex -output-directory=".\output\" -job-name="AdrianBergesCV_$(get-date -f MMMyyyy)"
+xelatex src/cv.tex `
+    -include-directory=".\src\" `
+    -output-directory=".\output\" `
+    -job-name="AdrianBergesCV_$(get-date -f MMMyyyy)"
