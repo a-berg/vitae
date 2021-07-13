ad513f299361f686961045db3a8c556714d1a558
Adrian Berges <aberges@outlook.com>
2021-07-13 13:13:44 +0200

    Changes in order to allow scribble preprocessing of dates and periods (Racket)

--- justfile
+++ justfile
@@ -1,12 +1,17 @@
 DATE := `date "+%b%Y"`
 JOBNAME := "AdrianBergesCV_" + DATE
 
-all:
-	xelatex \
-		-output-directory="./aux" \
-		src/cv.tex
-	mv ./aux/cv.pdf ./{{JOBNAME}}.pdf
+all: makepdf pythontex makepdf
+  mv ./aux/cv.pdf ./{{JOBNAME}}.pdf
 
+makepdf:
+  xelatex \
+    -output-directory="./aux" \
+    -interaction=nonstopmode \
+    src/cv.tex
+
+pythontex:
+  pythontex aux/cv.pytxcode
 
 get-fonts:
-	sudo apt install fonts-vollkorn fonts-open-sans
+  sudo apt install fonts-vollkorn fonts-open-sans
---
+++ pollen.rkt
@@ -0,0 +1,40 @@
+#lang racket/base
+
+(require gregor)
+(require gregor/period)
+(require racket/format)
+(require racket/list)
+(require racket/string)
+(require racket/match)
+
+(define (maybe-plural stem n)
+  (if (= n 1) stem
+      (~a stem
+          (if (string-suffix? stem "s")
+              "es"
+              "s"))))
+
+(define (non-empty p)
+  (filter-not (λ (pair) (zero? (cdr pair)))
+              (period->list p)))
+
+;; Quizás usar una macro? En realidad lo único que necesito "matchear" es la
+;; traducción.
+(define/match (period-elem->string tup)
+  [((cons 'years n)) (~a n " " (maybe-plural "año" n))]
+  [((cons 'months n)) (~a n " " (maybe-plural "mes" n))])
+
+(define (dates->period-string d1 d2)
+  (let ([period (period-between d1 d2 '(years months))])
+    (string-join
+     (map period-elem->string (non-empty period))
+     ", " #:before-last " y ")))
+
+(define (fmt-dates d1 d2)
+  (let ([d1str (~t d1 "yyyy")]
+        [d2str (if (date=? d2 (today)) "Actualidad" (~t d2 "yyyy"))])
+    (format "~a -- ~a (~a)" d1str d2str (dates->period-string d1 d2))))
+
+
+;; Aquí definir tests
+
